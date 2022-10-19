536310a7597ec39e358712715ed2f35fc9c946de
bussiere <bussiere@gmail.com>
2022-10-19 20:58:24 +0200

    wip master Updated: 2022-10-19 20:58:24

--- pyth/recap_sheet.py
+++ pyth/recap_sheet.py
@@ -45,25 +45,26 @@ target_img = target+".png"
 skull_img = skulls+".png"
 eye_img = eye+".png"
 pdf = FPDF(format="A4",orientation = 'P')
+pdf.set_auto_page_break(False, margin = 0.0)
 pdf.add_page()
 pdf.add_font("Firacode", "", "../assets/fira_code/testtfont.ttf", uni=True)
 pdf.set_font("Firacode", "", 8)
 pdf.set_xy(5, 1)
 #write A1 to T20
 grids = "A:1,B:2,C:3,D:4,E:5,F:6,G:7,H:8,I:9,J:10,K:11,L:12,M:13,N:14,O:15,P:16,Q:17,R:18,S:19,T:20"
-codes = "M=Move,B=begin,E=End,S=Scout,I=Inspect"
+codes = "M=Move,B=Begin,E=End,S=Scout,I=Inspect"
 codes2 ="ex: B:A1,M:A3,S:B4,M:B4,I:B4,M:B2,E:B2"
 pdf.multi_cell(0, 12/2, grids, 0, "L")
 pdf.set_font("Firacode", "", 8)
 pdf.set_xy(131, 5)
 pdf.multi_cell(0, 7, codes, 0, "L")
-pdf.set_xy(131, 10)
+pdf.set_xy(131, 8)
 pdf.multi_cell(0, 7, codes2, 0, "L")
 pdf.image(grid_image+".png", x=1, y=1,  w=120, h=120)
 pdf.image(grid_image+".png", x=1, y=118,  w=120, h=120)
 pdf.set_xy(125, 15)
 pdf.set_font("Firacode", "", 7)
-pdf.multi_cell(0, 12/2, "       Show where is the center of next cloud", 0, "L")
+pdf.multi_cell(0, 12/2, "       Show the center of next cloud (2d20)", 0, "L")
 pdf.set_xy(125, 21)
 pdf.multi_cell(0, 12/2, "       Activate the poison cloud", 0, "L")
 pdf.set_xy(125, 27)
@@ -72,6 +73,10 @@ pdf.image(target_img, x=125, y=15,  w=5, h=5)
 pdf.image(skull_img, x=125, y=21,  w=5, h=5)
 pdf.image(eye_img, x=125, y=27,  w=5, h=5)
 pdf.set_font("Firacode", "", 8)
+pdf.set_xy(195, 20)
+pdf.cell(w=10, h = 14, txt = "L/M P", border = 1, ln = 0,  align = 'C', fill = False, link = '')
+pdf.set_xy(195, 27)
+pdf.cell(w=10, h = 7, txt = "(30)", border = 0, ln = 0,  align = 'C', fill = False, link = '')
 i = 0
 start_y_cell = 34
 while (i < 26):
@@ -80,7 +85,10 @@ while (i < 26):
     pdf.cell(w=20, h = 7, txt = str(i+1), border = 1, ln = 0, 
             align = 'R', fill = False, link = '')
     pdf.set_xy(145, start_y_cell+i*7)
-    pdf.cell(w=60, h = 7, txt = '', border = 1, ln = 0, 
+    pdf.cell(w=50, h = 7, txt = '', border = 1, ln = 0, 
+            align = 'L', fill = False, link = '')
+    pdf.set_xy(195, start_y_cell+i*7)
+    pdf.cell(w=10, h = 7, txt = '', border = 1, ln = 0, 
             align = 'L', fill = False, link = '')
     if i+1 in [2,5,8,11,14,17,20,23]:
         pdf.image(target_img, x=127, y=start_y_cell+1+i*7,  w=4, h=4)
@@ -95,7 +103,7 @@ heigth_cell = 7
 start_cell_point_y = 218
 start_cell_point_x = 143
 cell_width = 65
-text_life_point = ["Action / Life Point","Start: 30 Point","1 Point = 2 Moves","1 Point = Inspect a Building", "1 Point = Scout a case at 1 distance","0 Point = Scout a case at 1 distance and End Turn","0 Point = Inspect longuely a building and End Turn"]
+text_life_point = ["Action / Life Point (Start at 30)","1 point = 1 Attack","1 Point = 2 Moves","1 Point = Inspect a Building", "1 Point = Scout a case at 1 distance","0 Point = Scout a case at 1 distance and End Turn","0 Point = Inspect longuely a building and End Turn"]
 i = 0
 while i < len(text_life_point):
     pdf.set_xy(start_cell_point_x, start_cell_point_y+i*heigth_cell)
@@ -114,48 +122,71 @@ pdf.image(range_2, x=175, y=270,  w=25, h=25)
 pdf.set_font("Firacode", "", 5)
 # Buildind roll
 #turn 1-2 on 1D6 : Residence,Manor, House,Shop,Casern
-turn1_2_building = ["Residence","Manor","House","Shop","Casern","Nothing"]
+turn1_2_building = ["Turn 1-12","Residence","Manor","House","Shop","Casern","Nothing"]
 i = 0
+start_x = 2
 start_y_cell = 240
-while i <6:
-    pdf.set_xy(2, start_y_cell+i*5)
-    pdf.cell(w=3, h = 5, txt = str(i+1), border = 1, ln = 0, 
-            align = 'R', fill = False, link = '')
-    pdf.set_xy(5, start_y_cell+i*5)
-    pdf.cell(w=12, h = 5, txt = turn1_2_building[i], border = 1, ln = 0, 
-            align = 'L', fill = False, link = '')
+h_cell=3
+while i <7:
+    j = i
+    if i == 0:
+        j=""
+        pdf.set_xy(start_x, start_y_cell+i*h_cell)
+        pdf.cell(w=12, h = h_cell, txt = turn1_2_building[i], border = 1, ln = 0, 
+                align = 'L', fill = False, link = '')
+    else:
+        pdf.set_xy(2, start_y_cell+i*h_cell)
+        pdf.cell(w=3, h = h_cell, txt = str(i), border = 1, ln = 0, 
+                align = 'R', fill = False, link = '')
+        pdf.set_xy(5, start_y_cell+i*h_cell)
+        pdf.cell(w=12, h = h_cell, txt = turn1_2_building[i], border = 1, ln = 0, 
+                align = 'L', fill = False, link = '')
     i+=1
 
-turn4_6_building = ["Residence","Shop","Casern","Nothing","Nothing","Nothing"]
+turn4_6_building = ["Turn 13-21","Residence","Shop","Casern","Nothing","Nothing","Nothing"]
 i = 0
 start_y_cell = 240
 start_x = 19
-while i <6:
-    pdf.set_xy(start_x , start_y_cell+i*5)
-    pdf.cell(w=3, h = 5, txt = str(i+1), border = 1, ln = 0, 
-            align = 'R', fill = False, link = '')
-    pdf.set_xy(start_x+3, start_y_cell+i*5)
-    pdf.cell(w=12, h = 5, txt = turn4_6_building[i], border = 1, ln = 0, 
-            align = 'L', fill = False, link = '')
+while i <7:
+    j = i
+    if i == 0:
+        j=""
+        pdf.set_xy(start_x, start_y_cell+i*h_cell)
+        pdf.cell(w=12, h = h_cell, txt = turn4_6_building[i], border = 1, ln = 0, 
+                align = 'L', fill = False, link = '')
+    else:
+        pdf.set_xy(start_x , start_y_cell+i*h_cell)
+        pdf.cell(w=3, h =h_cell, txt = str(i), border = 1, ln = 0, 
+                align = 'R', fill = False, link = '')
+        pdf.set_xy(start_x+3, start_y_cell+i*h_cell)
+        pdf.cell(w=12, h = h_cell, txt = turn4_6_building[i], border = 1, ln = 0, 
+                align = 'L', fill = False, link = '')
     i+=1
-turn7_building = ["House","Nothing","Nothing","Nothing","Nothing","Nothing"]
+turn7_building = ["Turn 22+","House","Nothing","Nothing","Nothing","Nothing","Nothing"]
 i = 0
 start_y_cell = 240
 start_x = 36
-while i <6:
-    pdf.set_xy(start_x , start_y_cell+i*5)
-    pdf.cell(w=3, h = 5, txt = str(i+1), border = 1, ln = 0, 
-            align = 'R', fill = False, link = '')
-    pdf.set_xy(start_x+3, start_y_cell+i*5)
-    pdf.cell(w=12, h = 5, txt = turn7_building[i], border = 1, ln = 0, 
-            align = 'L', fill = False, link = '')
+while i <7:
+    j = i
+    if i == 0:
+        j=""
+        pdf.set_xy(start_x, start_y_cell+i*h_cell)
+        pdf.cell(w=12, h = h_cell, txt = turn7_building[i], border = 1, ln = 0, 
+                align = 'L', fill = False, link = '')
+    else:
+        pdf.set_xy(start_x , start_y_cell+i*h_cell)
+        pdf.cell(w=3, h = h_cell, txt = str(i), border = 1, ln = 0, 
+                align = 'R', fill = False, link = '')
+        pdf.set_xy(start_x+3, start_y_cell+i*h_cell)
+        pdf.cell(w=12, h = h_cell, txt = turn7_building[i], border = 1, ln = 0, 
+                align = 'L', fill = False, link = '')
     i+=1
 
 
 Residence = ["Residence","Gun","Gun","Bat","Bat","Nothing","Nothing"]
 i = 0
 start_y_cell = 240
-start_x = 53
+start_x = 58
 h_cell = 3
 w_cell=12
 while i <7:
@@ -178,7 +209,7 @@ while i <7:
 Manor = ["Manor","4 Points","Shotgun","ChainSaw","Nothing","Nothing","Nothing"]
 i = 0
 start_y_cell = 240
-start_x = 70
+start_x = start_x+17
 h_cell = 3
 w_cell=12
 while i <7:
@@ -200,7 +231,7 @@ while i <7:
 House = ["House","4 Points","Gun","Knuckle","4 Points","Nothing","Nothing"]
 i = 0
 start_y_cell = 240
-start_x = 87
+start_x = start_x+17
 h_cell = 3
 w_cell=12
 while i <7:
@@ -223,7 +254,7 @@ while i <7:
 Shop = ["Shop","4 Points","Gun","Gun","4 Points","Binocular","Nothing"]
 i = 0
 start_y_cell = 240
-start_x = 104
+start_x = start_x+17
 h_cell = 3
 w_cell=12
 while i <7:
@@ -245,7 +276,7 @@ while i <7:
 Casern = ["Casern","4 Points","Shotgun","Gun","BowieKnife","Binocular","Nothing"]
 i = 0
 start_y_cell = 240
-start_x = 121
+start_x = start_x+17
 h_cell = 3
 w_cell=12
 while i <7:
@@ -264,14 +295,18 @@ while i <7:
                 align = 'L', fill = False, link = '')
     i+=1
 
-pdf.set_xy(2, 272)
-w_cell = 64
-h_cell =1
-pdf.cell(w=w_cell, h = h_cell, txt = "Inspect Longuely : Roll two times on the Building table", border = 0, ln = 0, 
-                align = 'L', fill = False, link = '')
-pdf.set_xy(64, 263)
-pdf.multi_cell(0, 3, "Gun : Range 2 ; Damage  2\nShotGun : Range 2 ; Damage  4\nChainSaw : Range 1 ; Damage  4\nBowieKnife : Range 1 ; Damage  3")
 
 pdf.set_xy(100, 263)
-pdf.multi_cell(0, 3, "Knuckle : Range 1 ; Damage  2\nBinocular : Scout at 1-2")
+pdf.multi_cell(0, 3, "Gun : Range 2 ; Damage  2\nShotGun : Range 2 ; Damage  4\nChainSaw : Range 1 ; Damage  4\nBowieKnife : Range 1 ; Damage  3\nKnuckle : Range 1 ; Damage  2\nBinocular : Scout at 1-2 \nNothing: Range 1; Damage 1")
+pdf.set_xy(2, 265)
+w_cell = 64
+h_cell =1
+pdf.set_font("Firacode", "", 5)
+pdf.multi_cell(0, 3, "Inspect Longuely : Roll two times on the Building table, only one time \nper building on the map\nScout : Determine a case at +1 and roll on the corresponding turn building table,\nplace the building on the map,other player must tell you if they are there.\nInspect a building: You roll on the corresponding building table. \nYou can roll only one time per building on the map.\nAttack : Attack on a case you can reach (by default 0-1 range,1 damage),\nWeapon modify the range and the damage.")
+pdf.set_xy(2, 290)
+w_cell = 64
+h_cell =1
+pdf.set_font("Firacode", "", 5)
+pdf.multi_cell(0, 3, "Inspect Longuely")
+
 pdf.output("../assets/print/recap_sheet.pdf", "F")
