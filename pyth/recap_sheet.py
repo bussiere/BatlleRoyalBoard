1e56228a94d130da60e8799092d840407d6f1e1a
bussiere <bussiere@gmail.com>
2022-10-25 17:45:45 +0200

    wip master Updated: 2022-10-25 17:45:45

--- pyth/recap_sheet.py
+++ pyth/recap_sheet.py
@@ -49,7 +49,7 @@ pdf = FPDF(format="A4",orientation = 'P')
 pdf.set_auto_page_break(False, margin = 0.0)
 pdf.add_page()
 pdf.add_font("Firacode", "", "../assets/fira_code/testtfont.ttf", uni=True)
-pdf.set_font("Firacode", "", 8)
+pdf.set_font("Firacode", "", 6)
 pdf.set_xy(5, 1)
 #write A1 to T20
 grids = "A:1,B:2,C:3,D:4,E:5,F:6,G:7,H:8,I:9,J:10,K:11,L:12,M:13,N:14,O:15,P:16,Q:17,R:18,S:19,T:20"
@@ -57,12 +57,15 @@ codes = "M=Move,B=Begin,E=End,S=Scout,I=Inspect"
 codes2 ="ex: B:A1,M:A3,S:B4,M:B4,I:B4,M:B2,E:B2"
 pdf.multi_cell(0, 12/2, grids, 0, "L")
 pdf.set_font("Firacode", "", 8)
-pdf.set_xy(131, 5)
+pdf.set_xy(131, 1)
 pdf.multi_cell(0, 7, codes, 0, "L")
-pdf.set_xy(131, 8)
+pdf.set_xy(131, 5)
 pdf.multi_cell(0, 7, codes2, 0, "L")
 pdf.image(grid_image+".png", x=1, y=0.5,  w=120, h=120)
 pdf.image(grid_image+".png", x=1, y=116,  w=120, h=120)
+pdf.set_xy(125, 11)
+pdf.set_font("Firacode", "", 6)
+pdf.multi_cell(0, 12/2, "           Before anyone play :", 0, "L")
 pdf.set_xy(125, 15)
 pdf.set_font("Firacode", "", 7)
 pdf.multi_cell(0, 12/2, "       Show the center of next cloud (2d20) [1]", 0, "L")
@@ -126,15 +129,15 @@ pdf.image(range_2, x=175, y=270,  w=25, h=25)
 pdf.set_font("Firacode", "", 5)
 # Buildind roll
 #turn 1-2 on 1D6 : Residence,Manor, House,Shop,Casern
-pdf.set_xy(15, 237)
+pdf.set_xy(15, 233)
 pdf.set_font("Firacode", "", 5)
 pdf.multi_cell(0, 3, "Turn Building Table")
-pdf.set_xy(87, 237)
+pdf.set_xy(87, 233)
 pdf.multi_cell(0, 3, "Item Building Table")
 turn1_2_building = ["Turn 1-12","Residence","Manor","House","Shop","Casern","Nothing"]
 i = 0
 start_x = 2
-start_y_cell = 240
+start_y_cell = 238
 h_cell=3
 while i <7:
     j = i
@@ -154,7 +157,6 @@ while i <7:
 
 turn4_6_building = ["Turn 13-21","Residence","Shop","Casern","Nothing","Nothing","Nothing"]
 i = 0
-start_y_cell = 240
 start_x = 19
 while i <7:
     j = i
@@ -173,7 +175,6 @@ while i <7:
     i+=1
 turn7_building = ["Turn 22+","House","Nothing","Nothing","Nothing","Nothing","Nothing"]
 i = 0
-start_y_cell = 240
 start_x = 36
 while i <7:
     j = i
@@ -194,7 +195,6 @@ while i <7:
 
 Residence = ["Residence","Gun","Gun","Bat","Bat","Nothing","Nothing"]
 i = 0
-start_y_cell = 240
 start_x = 58
 h_cell = 3
 w_cell=12
@@ -217,7 +217,6 @@ while i <7:
 
 Manor = ["Manor","4 A/L#P","Shotgun","ChainSaw","Nothing","Nothing","Nothing"]
 i = 0
-start_y_cell = 240
 start_x = start_x+17
 h_cell = 3
 w_cell=12
@@ -239,7 +238,6 @@ while i <7:
 
 House = ["House","4 A/L#P","Gun","Knuckle","4 A/L#P","Nothing","Nothing"]
 i = 0
-start_y_cell = 240
 start_x = start_x+17
 h_cell = 3
 w_cell=12
@@ -262,7 +260,6 @@ while i <7:
 
 Shop = ["Shop","4 A/L#P","Gun","Gun","4 A/L#P","Binocular","Nothing"]
 i = 0
-start_y_cell = 240
 start_x = start_x+17
 h_cell = 3
 w_cell=12
@@ -284,7 +281,6 @@ while i <7:
 
 Casern = ["Casern","4 A/L#P","Shotgun","Gun","BowieKnife","Binocular","Nothing"]
 i = 0
-start_y_cell = 240
 start_x = start_x+17
 h_cell = 3
 w_cell=12
@@ -305,16 +301,16 @@ while i <7:
     i+=1
 
 
-pdf.set_xy(100, 263)
-pdf.multi_cell(0, 3, "Gun : Range 2 ; Damage  2\nShotGun : Range 2 ; Damage  4\nChainSaw : Range 1 ; Damage  4\nBowieKnife : Range 1 ; Damage  3\nKnuckle : Range 1 ; Damage  2\nBinocular : Scout at 1-2 \nNothing: Range 1; Damage 1")
-pdf.set_xy(2, 263)
+pdf.set_xy(100, 260)
+pdf.multi_cell(0, 3, "Gun : Range 2 ; Damage  2\nShotGun : Range 2 ; Damage  4\nChainSaw : Range 1 ; Damage  4\nBowieKnife : Range 1 ; Damage  3\nKnuckle : Range 1 ; Damage  2\nBinocular : Scout at 1-2 \nNothing: Range 1; Damage 1\nDamage are taken from  A/L#P, \ndeath at 0 or in poison cloud.")
+pdf.set_xy(2, 260)
 w_cell = 64
 h_cell =1
 pdf.set_font("Firacode", "", 5)
-pdf.multi_cell(0, 3, "Inspect Longuely : Roll two times on the Item Building table, only one time \nper building on the map\nScout : Determine a case at +1 and roll on the corresponding Turn Building Table,\nplace the building on the map,other player must tell you if they are there.\nInspect a building: You roll on the corresponding Item Building Table. \nYou can roll only one time per building on the map.\nAttack : Attack on a case you can reach (by default 0-1 range,1 damage),\nWeapon modify the range and the damage.")
+pdf.multi_cell(0, 3, "Inspect and Scout actions are Public, Moves are not \nInspect Longuely : Roll two times on the Item Building table, only one time \nper building/player on the map\nScout : Determine a case at +1 and roll on the corresponding Turn Building Table,\nplace the building on the map,other player must tell you if they are there.\nInspect a building: You roll on the corresponding Item Building Table. \nOnly one time per building/player on the map.\nAttack : Attack on a case you can reach (by default 0-1 range,1 damage),\nWeapon modify the range and the damage.")
 pdf.set_xy(2, 287)
 pdf.set_font("Firacode", "", 5)
 pdf.image(target_img, x=12, y=287,  w=img_icon_size/2, h=img_icon_size/2)
-pdf.multi_cell(0, 3, "[1] Each     Turn , before anyone play, roll 2D20 (one for Letter other for Number) to determine where the center of the cloud will move,\nafter Turn 14 roll 2D10,starting with the leftest Letter and Number outside the cloud")
+pdf.multi_cell(0, 3, "[1] Each    Turn , before anyone play, roll 2D20 (one for Letter one for Number) to determine\nwhere the center of the cloud will move,after Turn 14 roll 2D10,\nstarting with the leftest Letter and Number outside the cloud")
 
 pdf.output("../assets/print/recap_sheet.pdf", "F")
